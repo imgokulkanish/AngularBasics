ebbcd954a5ecb409ad1dcbd5915a1d73e32e56c3
Gokul Kanish <66904387+imgokulkanish@users.noreply.github.com>
2022-01-09 14:13:33 +0530

    bindings updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +18,8 @@ import { SidecomponentComponent } from './sidecomponent/sidecomponent.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,9 +10,25 @@ export class HeaderComponent implements OnInit {
   //String Interpolation
   channelId : number = 1;
   channelName: string ="Unpopular Cricket"
-  constructor() { }
+  //Property Binding
+  channelActive: boolean = false;
+  //Event Binding
+  channelMember: number = 2;
+  AddChannelMember(){
+    this.channelMember++;
+ }
+ //Event Binding with Parameter
 
+ inputEvent(event:any){
+   console.log(event.target.value);
+ }
+ //Twowaybinding
+ channelWebsite: string ="UnpopularCricket"
+  constructor() { 
+   setTimeout(() => {
+     this.channelActive = true;
+   }, 5000);
+  }
   ngOnInit(): void {
   }
-
 }
